1e616d72aac4f9ad718a3537f88f558adc28c247
neha <neha7895928593@gmail.com>
2026-08-22 16:58:59 +0530

    bug fix

--- Backend/server.js
+++ Backend/server.js
@@ -39,7 +39,7 @@ app.use("/api", datasetRoutes);
 app.use("/api/ai", aiRoutes);
 
 
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
